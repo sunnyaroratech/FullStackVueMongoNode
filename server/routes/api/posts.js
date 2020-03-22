802e728040392ba7712414d3983de8079c74e031
Sandeep Shahi (DHL CH) <Sandeep.Shahi@dhl.com>
2020-03-22 20:55:19 +0100

    Intial backend created

---
+++ server/routes/api/posts.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongodb = require ('mongodb');
+
+const router = express.Router();
+
+// GET POSTS 
+router.get('/', async(req, res)=> {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+//ADD POSTS
+router.post('/', async (req,res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date(),
+    });
+    res.status(201).send();
+}
+    );
+
+//DELETE POSTS 
+router.delete('/:id', async (req,res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id) });
+    res.status(200).send();
+});
+
+
+async function loadPostsCollection(){
+const client = await mongodb.MongoClient.connect(
+    'mongodb+srv://abc123:Sandeep123@cluster0-sdzxn.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser:true}
+);
+return client.db('test').collection('posts');
+}
+module.exports = router; 
